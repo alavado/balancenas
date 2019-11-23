d2e6f31b8f1f3884e1f5856ac468f366e6581cf6
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-11-23 17:44:58 -0300

    agrega esquema graphql

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Cenas from '../Cenas'
 
 function App() {
   return (
-    <div>Balancenas</div>
+    <>
+      <div>Balancenas</div>
+      <BrowserRouter>
+        <Route path="/" exact component={Cenas} />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Cenas/Cenas.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cenas.css'
+
+const Cenas = () => {
+  return (
+    <div>
+      Cenas
+    </div>
+  )
+}
+
+export default Cenas
